95b24bc94687304d35fca10a39ae4707ba1422d9
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-07 15:58:34 +0300

    updated: lambda

--- app/src/main/java/com/ismailmesutmujde/kotlincomposeintro/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlincomposeintro/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,16 +19,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            KotlinComposeIntroTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContent{
+            Greeting(name = "Android")
         }
+
+    }
+
+    fun testFunction(int: Int, myfunction: ()->Unit) {
+        myfunction.invoke()
+    }
+
+    fun testFunctionLambda() {
+        println("test")
     }
 }
 
