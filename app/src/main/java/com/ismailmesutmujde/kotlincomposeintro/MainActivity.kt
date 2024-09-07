abc58f1ff9f606d788ed3d3f2f431c1752e4f9d8
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-07 17:10:02 +0300

    updated: alignment

--- app/src/main/java/com/ismailmesutmujde/kotlincomposeintro/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlincomposeintro/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,9 +23,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,10 +50,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     // Column, Row, Box
 
-    Column {
-        CustomText(text = "Hello Android")
-        CustomText(text = "Hello World")
-        CustomText(text = "Hello Kotlin")
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.DarkGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CustomText(text = "Hello Android!")
+        Spacer(modifier =Modifier.padding(5.dp))
+        CustomText(text = "Hello World!")
+        Spacer(modifier =Modifier.padding(5.dp))
+        CustomText(text = "Hello Kotlin!")
     }
 
 
@@ -111,20 +122,21 @@ fun CustomText(text: String) {
     Text(
         text = text,
         modifier = Modifier
-            .background(color = Color.Black)
-            .padding(top = 10.dp, start = 1.dp, end = 1.dp, bottom = 30.dp)
             .clickable {
                 println("hello android clicked")
             }
-            //.width(250.dp)
+            .background(color = Color.Black)
+            .padding(top = 10.dp, start = 1.dp, end = 1.dp, bottom = 30.dp)
+            .width(200.dp)
             //.height(200.dp)
             //.size(width = 50.dp, height = 50.dp)
             //.fillMaxWidth(0.5f)
             //.fillMaxHeight(0.5f)
-            .fillMaxSize(0.5f),
-        color = Color.White,
+            //.fillMaxSize(0.5f)
+        ,color = Color.White,
         fontSize = 22.sp,
-        fontWeight = FontWeight.Bold
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
     )
 }
 
